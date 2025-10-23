fc89132c47d3401848df5de216dbf746f23ae6f8
theamirm <theamirm@gmail.com>
2025-10-24 02:33:44 +0330

    feat: add Vue-based webview, HelloWorldPanel and dev tooling

---
+++ webview-ui/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare const acquireVsCodeApi: () => ({
+  postMessage: (message: unknown) => void,
+  getState: () => unknown,
+  setState: <T>(state: T) => T,
+})
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
+  }
+}
